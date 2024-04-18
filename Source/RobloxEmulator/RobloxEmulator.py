1d75b62a3f39a34ac6b7046a06eb38c47607a900
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-04-18 22:09:47 +0200

    Added support for functions

--- Source/RobloxEmulator/RobloxEmulator.py
+++ Source/RobloxEmulator/RobloxEmulator.py
@@ -2,6 +2,10 @@ from APIDumper import APIDumper
 from LuaWriter import LuaWriter
 import os
 from jinja2 import Environment, FileSystemLoader, Template
+import re
+
+def has_whitespace(string):
+    return bool(re.search(r'\s', string))
 
 class RobloxEmulator:
     def __init__(self) -> None:
@@ -14,7 +18,12 @@ if __name__ == "__main__":
     emulator = RobloxEmulator()
     classes = emulator.Dumper.Data()["Classes"]
     temp = emulator.Env.get_template("classes.lua.template")
-    data = {"classes" : classes}
+    for cl in classes:
+        for member in cl["Members"]:
+            if has_whitespace(member["Name"]):
+                member["Name"] = f'["{member["Name"]}"]'
+    data = {"classes" : classes,
+            "len" : len}
     output = temp.render(data)
     with open(os.path.join(os.path.dirname(__file__), "classes.lua"), "w") as f:
         f.write(output)
