3a8ffbdcf2dc5206d52586f7d28ca6dd61f4d8ac
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-04-18 22:17:40 +0200

    forgot int64 and fixed write_params

--- Source/RobloxEmulator/RobloxEmulator.py
+++ Source/RobloxEmulator/RobloxEmulator.py
@@ -13,6 +13,17 @@ class RobloxEmulator:
         self.Writer = LuaWriter(self.Dumper.Data())
         self.TemplateLoader = FileSystemLoader(searchpath=os.path.join(os.path.dirname(__file__), "templates"))
         self.Env = Environment(loader=self.TemplateLoader)
+def write_params(params):
+    if len(params) == 0:
+        return ""
+    if len(params) == 1:
+        return params[0]["Name"] + ": " + params[0]["Type"]["Name"]
+    out = ""
+    for idx, param in enumerate(params[:-1]):
+        out += param["Name"] + ": " + param["Type"]["Name"] + ", "
+    out += params[-1]["Name"]
+
+    return out
 
 if __name__ == "__main__":
     emulator = RobloxEmulator()
@@ -23,7 +34,7 @@ if __name__ == "__main__":
             if has_whitespace(member["Name"]):
                 member["Name"] = f'["{member["Name"]}"]'
     data = {"classes" : classes,
-            "len" : len}
+            "write_params" : write_params}
     output = temp.render(data)
     with open(os.path.join(os.path.dirname(__file__), "classes.lua"), "w") as f:
         f.write(output)
